8be4b17205a335ce88cd2d61128f5d30fe17f1ed
amatoryduck <alexander.n.scott@gmail.com>
2019-10-30 23:10:12 -0400

    first commit

---
+++ goodMorning.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "saying good morning"
+osascript sendMessage.scpt +13014374472 'good morning'
+osascript sendMessage.scpt +13014378533 'bom dia'
---
+++ goodNight.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "saying goodnight"
+osascript sendMessage.scpt +13014374472 'goodnight'
+osascript sendMessage.scpt +13014378533 'boa noite'

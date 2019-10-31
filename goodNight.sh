934c6ba4b637c3d3eebdfef3c04953df14addb73
amatoryduck <alexander.n.scott@gmail.com>
2019-10-30 23:11:35 -0400

    Update goodNight.sh

--- goodNight.sh
+++ goodNight.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "saying goodnight"
-osascript sendMessage.scpt +13014374472 'goodnight'
-osascript sendMessage.scpt +13014378533 'boa noite'
+osascript sendMessage.scpt <MY PARENTS PHONE NUMBER> 'goodnight'
+osascript sendMessage.scpt <MY PARENTS PHONE NUMBER> 'boa noite'
